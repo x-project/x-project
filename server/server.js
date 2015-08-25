b4fef1213015fd0c4c7fdf957571e1195a345b7d
enricomarino <enrico.marino@email.com>
2015-08-25 18:27:01 +0200

    .

--- server/server.js
+++ server/server.js
@@ -1,22 +1,18 @@
+var path = require('path');
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var path = require('path');
 
 var app = module.exports = loopback();
 
-app.use(loopback.token({ model: app.models.accessToken }));
+app.use(loopback.favicon());
 app.use(loopback.compress());
 
 boot(app, __dirname);
 
 app.use(loopback.static(path.resolve(__dirname, '../client')));
-app.use(loopback.static(path.resolve(__dirname, '../common')));
 
-app.use(loopback.json());
-app.use(loopback.urlencoded({ extended: true }));
-
-app.index_file_path = path.resolve(__dirname, '../client/index.html');
-app.get('*', function (req, res) { res.sendFile(app.index_file_path); });
+var index_path = path.resolve(__dirname, '../client/index.html');
+app.get('*', function (req, res) { res.sendFile(index_path); });
 
 app.start = function() {
   return app.listen(function() {
